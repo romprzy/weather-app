2828a03c1626f52d3e9efceee2687f7f64814d9f
Roman Przybylski <romprzy@o2.pl>
2019-01-16 13:25:10 +0100

    Some visual setup + basic local storage/status setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="app">
-    <h1>{{ title }}</h1>
-    <div v-if="message">{{ message }}</div>
-    <DisplayWeather v-if="weather" :weather="this.weather" />
+  <div id="app" class="text-center">
+    <h1 class="text-secondary">{{ title }}</h1>
+
+    <div v-if="message" class="alert alert-danger">
+      {{ message }}<br>
+      Please try again later...
+    </div>
+    <DisplayWeather v-if="weather" :weather="this.weather" :offline="offline" />
+
+    <div class="status mt-4 text-left text-secondary">
+      <span class="status-icon" :class="{'bg-success': !offline, 'bg-warning': offline}"></span>
+      <span v-if="offline">Offline</span>
+      <span v-else>Online</span>
+    </div>
+
+    <div class="spinner" v-if="loading">
+      <div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div>
+    </div>
   </div>
 </template>
 
 <script>
-import DisplayWeather from './components/DisplayWeather.vue'
+import DisplayWeather from './components/DisplayWeather.vue';
 
 export default {
   name: 'app',
@@ -17,30 +31,22 @@ export default {
   data () {
     return {
       title: 'Weather app',
+      loading: false,
+      offline: true,
+      message: false,
       api: {
         base_url: 'http://api.openweathermap.org/data/2.5/weather',
         appid: 'ade7a099018e4beaa468a7b3dbab2c04'
       },
-      lat: false,
-      lon: false,
-      city: false,
-      country_code: false,
-      q: false,
-      message: false,
-      body: false,
+      location: {},
       weather: false
     }
   },
   computed: {
     url () {
-      let location = '';
-      if (this.lat && this.lon) {
-        location = `lat=${this.lat}&lon=${this.lon}`;
-      } else if (this.city) {
-        location = `q=${this.city}`
-        if (this.country_code) {
-          location += `,${this.country_code}`
-        }
+      let location = {};
+      if (this.location && this.location.lat && this.location.lon) {
+        location = `lat=${this.location.lat}&lon=${this.location.lon}`;
       }
 
       return `${this.api.base_url}?appid=${this.api.appid}&${location}`;
@@ -51,50 +57,108 @@ export default {
       if (navigator && 'geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(
           position => {
-            console.log('position', position);
-            this.lat = position.coords.latitude;
-            this.lon = position.coords.longitude;
-
-            this.getWeather({ lat: position.coords.latitude, lon: position.coords.longitude });
+            this.location = {
+              lat: position.coords.latitude,
+              lon: position.coords.longitude
+            }
           },
           error => {
             this.message = error.message;
           }
         );
       } else {
-        this.message = 'Geolocation is not available. Please type in latitude and longitude';
+        this.message = 'Geolocation is not available on this device.';
       }
     },
-    getWeather ({ lat, lon }) {
+    getWeather () {
+      this.loading = true;
       fetch (this.url)
         .then(response => {
-          console.log('weather', response);
           return response.json();
         })
         .then(body => {
-          console.log('body', body);
           this.weather = body;
+          this.saveToLocalStorage('weather', body);
+          this.loading = false;
+          this.offline = false;
         })
         .catch (error => {
-          console.log('error', error);
+          this.loading = false;
+          if (this.getFromLocalStorage('weather')) {
+            this.weather = this.getFromLocalStorage('weather');
+            this.offline = true;
+          }
           if ('message' in error) {
+            console.log('getWeather Wrror');
             this.message = error.message;
           }
         })
+    },
+    getFromLocalStorage (item) {
+      const data = JSON.parse(localStorage.getItem(item));
+      return data;
+    },
+    saveToLocalStorage (item, data) {
+      localStorage.setItem(item, data);
     }
   },
   created () {
     this.getLocation();
+  },
+  mounted () {
+    window.addEventListener('getWeatherError', () => {
+      console.log('getWeatherError event');
+      this.weather = this.getFromLocalStorage('weather');
+    });
+  },
+  watch: {
+    location () {
+      this.getWeather(this.location);
+      this.saveToLocalStorage ('location', JSON.stringify(this.location));
+    },
+    weather () {
+      this.saveToLocalStorage ('weather', JSON.stringify(this.weather));
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
-}
+  body {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    font-weight: 300;
+  }
+
+  h1 {
+    font-weight: 300;
+    font-size: 2.5rem;
+    margin-bottom: 1em;
+  }
+
+  .spinner {
+    position: fixed;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(0, 0, 0, .75);
+    transition: background .35s;
+  }
+
+  .status-icon {
+    display: inline-block;
+    border-radius: 50%;
+    width: .75rem;
+    height: .75rem;
+    margin-right: .5em;
+    vertical-align: baseline;
+  }
 </style>
--- src/components/DisplayWeather.vue
+++ src/components/DisplayWeather.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="weather">
-    <h2 v-if="weather.name">{{ weather.name }}</h2>
+    <h2 v-if="weather.name" class="city">{{ weather.name }}</h2>
     <div>
-      <img v-if="icon" :src="`http://openweathermap.org/img/w/${icon}.png`" :alt="description" >
-      <div v-if="description">{{ description }}</div>
-      <div v-if="temp" v-html="temp"></div>
-      <div v-if="pressure">{{ pressure }}</div>
+      <p v-if="temp" class="temp text-primary">{{ temp }}</p>
+      <p v-if="description">
+        <img v-if="icon" :src="`http://openweathermap.org/img/w/${icon}.png`" :alt="description" class="weather-icon" width="50" height="50">
+        <span class="weather-description pl-4 text-light">{{ description }}</span>
+      </p>
+      <p v-if="pressure" class="pressure weather-block">
+        <span class="weather-label text-light">
+          Pressure:
+        </span>
+        <span class="weather-value">
+          {{ pressure }}
+        </span>
+      </p>
+      <p v-if="windSpeed" class="weather-block">
+        <span class="weather-label text-light">
+        Wind:
+        </span>
+        <span class="weather-value weather-value-wind wind">
+          {{ windSpeed }}
+          <span v-if="windDirection" class="wind-direction">
+            <span class="wind-direction-arrow" :style="`transform: translate(-50%, -50%) rotate(${windDirection}deg)`">&darr;</span>
+          </span>
+        </span>
+      </p>
     </div>
+    <!-- <button class="btn btn-primary">Save for offline use</button> -->
   </div>
 </template>
 
@@ -29,47 +50,70 @@ export default {
       return this.weather.main.humidity;
     },
     temp () {
-      return Number(this.weather.main.temp) - 272.15 + '&deg;C';
+      return Number(this.weather.main.temp) - 272.15 + '°C';
     },
     pressure () {
       return this.weather.main.pressure + ' hPa';
     },
-  },
-  data () {
-    return {
-    }
-  },
-  mounted () {
-    console.log(this.weather);
+    windSpeed () {
+      return `${this.weather.wind.speed} m/s`;
+    },
+    windDirection () {
+      return this.weather.wind.deg;
+    },
   }
 }
 </script>
 
 <style lang="scss">
-body {
-  background: #333;
-  color: #fff;
-}
+  $gray: #999;
 
-.weather {
-  border: 1px solid rgba(255, 255, 255, .25);
-  display: inline-flex;
-  flex-direction: column;
-  padding: 20px 40px;
-}
+  .weather {
+    border: 1px solid rgba(255, 255, 255, .2);
+    display: inline-flex;
+    flex-direction: column;
+    padding: 20px 40px;
 
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    &-block {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
+
+  .city {
+    font-size: 2.5rem;
+    font-weight: 300;
+  }
+
+  .temp {
+    font-size: 3rem;
+    margin: 0;
+  }
+
+  .wind {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    &-direction {
+      position: relative;
+      display: inline-block;
+      width: 1em;
+      height: 1em;
+      margin-left: .5em;
+
+      &-arrow {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        font-size: 1.5em;
+      }
+    }
+  }
+
+  h2, p {
+    margin: .25em 0;
+  }
 </style>
